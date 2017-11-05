e753b98a558403f758e21dccef113e647ca14b78
Alberto Spelta <alberto.spelta@live.com>
2017-11-05 22:38:11 +0100

    Automate path mapping for LocalDB AttachDBFilename

--- Demos/Utilities/Helper.cs
+++ Demos/Utilities/Helper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace Demos
 {
@@ -13,10 +14,13 @@ namespace Demos
 
         static Helper()
         {
+            var data = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.GetDirectories("Data").Single();
+            AppDomain.CurrentDomain.SetData("DataDirectory", data.FullName);
+
             var builder = new SqlConnectionStringBuilder()
             {
                 DataSource = @"(LocalDB)\MSSQLLocalDB",
-                AttachDBFilename = @"d:\Work\Sensoria\Conferences\PASS\2017\Dapper\Demo\Data\DapperSample.mdf",
+                AttachDBFilename = @"|DataDirectory|\DapperSample.mdf",
                 IntegratedSecurity = true,
                 ConnectTimeout = 30,
                 ApplicationName = "DapperDemo"
